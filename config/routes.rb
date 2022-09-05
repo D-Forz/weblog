94b0046bcc0b77ed8aafcacf11eb97d92aa707cf
duvan <duvanfoz@hotmail.com>
2022-09-05 15:20:04 -0500

    add controller pages and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # Verb 'path', to: 'controller#action'
+
+  # root
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
