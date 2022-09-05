d517a890507dfc839b424d56c3e7a49828314a89
duvan <duvanfoz@hotmail.com>
2022-09-05 16:00:58 -0500

    add some html - erb code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+
+  POSTS = {
+    '1' => { title: 'First Post', body: 'This is the first post' },
+    '2' => { title: 'Second Post', body: 'This is the second post' },
+    '3' => { title: 'Third Post', body: 'This is the third post' }
+  }
+
+  def index
+    @posts = POSTS
+  end
 end
